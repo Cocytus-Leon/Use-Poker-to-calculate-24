b6e42a2387541239ebd7494bf47a8756fe2c2ba0
Cocytus-Leon <65547713+Cocytus-Leon@users.noreply.github.com>
2021-03-22 11:40:54 +0800

    Add files via upload

---
+++ 24点/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class 巧算24点
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnstart = New System.Windows.Forms.Button()
+        Me.NUM1 = New System.Windows.Forms.RichTextBox()
+        Me.NUM2 = New System.Windows.Forms.RichTextBox()
+        Me.NUM3 = New System.Windows.Forms.RichTextBox()
+        Me.NUM4 = New System.Windows.Forms.RichTextBox()
+        Me.btncount = New System.Windows.Forms.Button()
+        Me.Ans = New System.Windows.Forms.ListBox()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnstart
+        '
+        Me.btnstart.Location = New System.Drawing.Point(158, 395)
+        Me.btnstart.Name = "btnstart"
+        Me.btnstart.Size = New System.Drawing.Size(70, 35)
+        Me.btnstart.TabIndex = 0
+        Me.btnstart.Text = "发牌"
+        Me.btnstart.UseVisualStyleBackColor = True
+        '
+        'NUM1
+        '
+        Me.NUM1.Font = New System.Drawing.Font("幼圆", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.NUM1.ForeColor = System.Drawing.Color.Red
+        Me.NUM1.Location = New System.Drawing.Point(33, 8)
+        Me.NUM1.Name = "NUM1"
+        Me.NUM1.Size = New System.Drawing.Size(155, 170)
+        Me.NUM1.TabIndex = 1
+        Me.NUM1.Text = ""
+        '
+        'NUM2
+        '
+        Me.NUM2.Font = New System.Drawing.Font("幼圆", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.NUM2.ForeColor = System.Drawing.Color.Blue
+        Me.NUM2.Location = New System.Drawing.Point(203, 8)
+        Me.NUM2.Name = "NUM2"
+        Me.NUM2.Size = New System.Drawing.Size(155, 170)
+        Me.NUM2.TabIndex = 2
+        Me.NUM2.Text = ""
+        '
+        'NUM3
+        '
+        Me.NUM3.Font = New System.Drawing.Font("幼圆", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.NUM3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.NUM3.Location = New System.Drawing.Point(33, 206)
+        Me.NUM3.Name = "NUM3"
+        Me.NUM3.Size = New System.Drawing.Size(155, 170)
+        Me.NUM3.TabIndex = 3
+        Me.NUM3.Text = ""
+        '
+        'NUM4
+        '
+        Me.NUM4.Font = New System.Drawing.Font("幼圆", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.NUM4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.NUM4.Location = New System.Drawing.Point(203, 206)
+        Me.NUM4.Name = "NUM4"
+        Me.NUM4.Size = New System.Drawing.Size(155, 170)
+        Me.NUM4.TabIndex = 5
+        Me.NUM4.Text = ""
+        '
+        'btncount
+        '
+        Me.btncount.Location = New System.Drawing.Point(381, 133)
+        Me.btncount.Name = "btncount"
+        Me.btncount.Size = New System.Drawing.Size(70, 36)
+        Me.btncount.TabIndex = 6
+        Me.btncount.Text = "计算"
+        Me.btncount.UseVisualStyleBackColor = True
+        '
+        'Ans
+        '
+        Me.Ans.Font = New System.Drawing.Font("幼圆", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Ans.ItemHeight = 18
+        Me.Ans.Location = New System.Drawing.Point(472, 12)
+        Me.Ans.Name = "Ans"
+        Me.Ans.Size = New System.Drawing.Size(316, 364)
+        Me.Ans.TabIndex = 7
+        '
+        'btnclear
+        '
+        Me.btnclear.Location = New System.Drawing.Point(543, 395)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(70, 35)
+        Me.btnclear.TabIndex = 8
+        Me.btnclear.Text = "清空"
+        Me.btnclear.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(381, 175)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(32, 32)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "+"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(419, 175)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(32, 32)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "-"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(381, 213)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(32, 32)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "*"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(419, 213)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(32, 32)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "/"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(721, 405)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(22, 15)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "？"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(649, 405)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 15)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "解的个数"
+        '
+        '巧算24点
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.Ans)
+        Me.Controls.Add(Me.btncount)
+        Me.Controls.Add(Me.NUM4)
+        Me.Controls.Add(Me.NUM3)
+        Me.Controls.Add(Me.NUM2)
+        Me.Controls.Add(Me.NUM1)
+        Me.Controls.Add(Me.btnstart)
+        Me.Name = "巧算24点"
+        Me.Text = "巧算24点"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnstart As Button
+    Friend WithEvents NUM1 As RichTextBox
+    Friend WithEvents NUM2 As RichTextBox
+    Friend WithEvents NUM3 As RichTextBox
+    Friend WithEvents NUM4 As RichTextBox
+    Friend WithEvents btncount As Button
+    Friend WithEvents Ans As ListBox
+    Friend WithEvents btnclear As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ 24点/Form1.vb
@@ -0,0 +1,282 @@
+﻿Public Class 巧算24点
+
+    Dim AnswerN As Integer
+    Dim equation2 As String
+    Dim equation1 As String
+    Public Function Count3(ByVal a As Integer, b As Integer)
+        '作为三级，由从二级得到的两个数通过加减乘除判断是否满足条件，满足则输出满足结果的等式组
+        '两数相加等于24的情况
+        If (a + b = 24) Then
+            AnswerN = AnswerN + 1
+            Ans.Items.Add(equation1 + "  " + equation2 + "  " + CStr(a) + "+" + CStr(b) + " =" + CStr(24))
+        End If
+        '两数相乘等于24的情况
+        If (a * b = 24) Then
+            AnswerN = AnswerN + 1
+            Ans.Items.Add(equation1 + "  " + equation2 + "  " + CStr(a) + "*" + CStr(b) + " =" + CStr(24))
+        End If
+        '两数相减等于24的情况
+        If (a - b = 24) Then
+            AnswerN = AnswerN + 1
+            Ans.Items.Add(equation1 + "  " + equation2 + "  " + CStr(a) + "-" + CStr(b) + " =" + CStr(24))
+        ElseIf (b - a = 24) Then
+            AnswerN = AnswerN + 1
+            Ans.Items.Add(equation1 + "  " + equation2 + "  " + CStr(b) + "-" + CStr(a) + " =" + CStr(24))
+        End If
+        '两数相除等于24的情况
+        If ((b <> 0) And (a = 24 * b)) Then
+            AnswerN = AnswerN + 1
+            Ans.Items.Add(equation1 + "  " + equation2 + "  " + CStr(a) + "/" + CStr(b) + " =" + CStr(24))
+        End If
+        If ((a <> 0) And (b = 24 * a)) Then
+            AnswerN = AnswerN + 1
+            Ans.Items.Add(equation1 + "  " + equation2 + "  " + CStr(b) + "/" + CStr(a) + " =" + CStr(24))
+        End If
+    End Function
+
+    Public Function Count2(ByVal a As Integer, b As Integer, c As Integer)
+        '作为二级，由从一级得到三个数，用两个通过加减乘除得到一个新数与一个等式，并将另一个数，新数与等式输出给三级
+        Dim value(2) As Integer
+        Dim i, j, k, newvalue As Integer
+        value(0) = a
+        value(1) = b
+        value(2) = c
+
+        For i = 0 To 2
+            For j = i + 1 To 2
+                If j > 2 Then
+                    Exit For
+                End If
+                k = 3 - i - j   '输出给上级的数
+                '两数相加得新数
+                newvalue = value(i) + value(j)
+                equation2 = CStr(value(i)) + "+" + CStr(value(j)) + " =" + CStr(newvalue) '//得到一个等式输给三级
+                Count3(newvalue, value(k)) '将新数和另一个数输出给三级
+                '两数相乘得新数
+                newvalue = value(i) * value(j)
+                equation2 = CStr(value(i)) + "*" + CStr(value(j)) + " =" + CStr(newvalue)
+                Count3(newvalue, value(k))
+                '两数相减得新数
+                If (value(i) > value(j)) Then
+                    newvalue = value(i) - value(j)
+                    equation2 = CStr(value(i)) + "-" + CStr(value(j)) + " =" + CStr(newvalue)
+                    Count3(newvalue, value(k))
+                Else
+                    newvalue = value(j) - value(i)
+                    equation2 = CStr(value(j)) + "-" + CStr(value(i)) + " =" + CStr(newvalue)
+                    Count3(newvalue, value(k))
+                End If
+                '两数相除得新数
+                If ((value(j) <> 0) And (value(i) Mod value(j) = 0)) Then
+                    newvalue = value(i) / value(j)
+                    equation2 = CStr(value(i)) + "/" + CStr(value(j)) + " =" + CStr(newvalue)
+                    Count3(newvalue, value(k))
+                ElseIf ((value(i) <> 0) And (value(j) Mod value(i) = 0)) Then
+                    newvalue = value(j) / value(i)
+                    equation2 = CStr(value(j)) + "/" + CStr(value(i)) + " =" + CStr(newvalue)
+                    Count3(newvalue, value(k))
+                End If
+            Next j
+        Next i
+    End Function
+
+    Public Function Count1(ByVal a As Integer, b As Integer, c As Integer, d As Integer)
+        '作为一级，由输入的四个数，用两个通过加减乘除得到一个新数与一个等式，并将另两个数，新数输出给二级，等式输出给三级
+        Dim value(3) As Integer
+        Dim i, j, k, m, newvalue As Integer
+        value(0) = a
+        value(1) = b
+        value(2) = c
+        value(3） = d
+        For i = 0 To 3
+
+            For j = i + 1 To 3
+                If j > 3 Then
+                    Exit For
+                End If
+                'For k = 0 To ((k == i) || (k == j))'输出给中级的数
+                k = 0
+                Do While (k = i Or k = j)
+                    'for (k = 0;(k == i) || (k == j);k++)
+                    k = k + 1
+                Loop
+                m = 6 - i - j - k '//输出给中级的数
+                '//两数相加得新数
+                newvalue = value(i) + value(j)
+                equation1 = CStr(value(i)) + "+" + CStr(value(j)) + " =" + CStr(newvalue) ';//得到一个等式输给上级
+                Count2(newvalue, value(k), value(m)) ';//将新数和另两个数输出给中级
+                '//两数相乘得新数
+                newvalue = value(i) * value(j)
+                equation1 = CStr(value(i)) + "*" + CStr(value(j)) + " =" + CStr(newvalue)
+                Count2(newvalue, value(k), value(m))
+                '//两数相减得新数
+                If (value(i) > value(j)) Then
+                    newvalue = value(i) - value(j)
+                    equation1 = CStr(value(i)) + "-" + CStr(value(j)) + " =" + CStr(newvalue)
+                    Count2(newvalue, value(k), value(m))
+                Else
+                    newvalue = value(j) - value(i)
+                    equation1 = CStr(value(j)) + "-" + CStr(value(i)) + " =" + CStr(newvalue)
+                    Count2(newvalue, value(k), value(m))
+                End If
+                '//两数相除得新数
+                If ((value(j) <> 0) And (value(i) Mod value(j) = 0)) Then
+                    newvalue = value(i) / value(j)
+                    equation1 = CStr(value(i)) + "/" + CStr(value(j)) + " =" + CStr(newvalue)
+                    Count2(newvalue, value(k), value(m))
+                ElseIf ((value(i) <> 0) And (value(j) Mod value(i) = 0)) Then
+                    newvalue = value(j) / value(i)
+                    equation1 = CStr(value(j)) + "/" + CStr(value(i)) + " =" + CStr(newvalue)
+                    Count2(newvalue, value(k), value(m))
+                End If
+            Next j
+        Next i
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnstart.Click
+        Randomize()
+        NUM1.Text = 0
+        NUM2.Text = 0
+        NUM3.Text = 0
+        NUM4.Text = 0
+        Do Until (NUM1.Text <> NUM2.Text And NUM1.Text <> NUM3.Text And NUM1.Text <> NUM4.Text And NUM2.Text <> NUM3.Text And NUM2.Text <> NUM4.Text And NUM3.Text <> NUM4.Text)
+            NUM1.Text = Int(Rnd() * 13) + 1
+            NUM2.Text = Int(Rnd() * 13) + 1
+            NUM3.Text = Int(Rnd() * 13) + 1
+            NUM4.Text = Int(Rnd() * 13) + 1
+        Loop
+        If (NUM1.Text = 1) Then
+            NUM1.Text = "A"
+        ElseIf (NUM1.Text = 11) Then
+            NUM1.Text = "J"
+        ElseIf (NUM1.Text = 12) Then
+            NUM1.Text = "Q"
+        ElseIf (NUM1.Text = 13) Then
+            NUM1.Text = "K"
+        End If
+        If (NUM2.Text = 1) Then
+            NUM2.Text = "A"
+        ElseIf (NUM2.Text = 11) Then
+            NUM2.Text = "J"
+        ElseIf (NUM2.Text = 12) Then
+            NUM2.Text = "Q"
+        ElseIf (NUM2.Text = 13) Then
+            NUM2.Text = "K"
+        End If
+        If (NUM3.Text = 1) Then
+            NUM3.Text = "A"
+        ElseIf (NUM3.Text = 11) Then
+            NUM3.Text = "J"
+        ElseIf (NUM3.Text = 12) Then
+            NUM3.Text = "Q"
+        ElseIf (NUM3.Text = 13) Then
+            NUM3.Text = "K"
+        End If
+        If (NUM4.Text = 1) Then
+            NUM4.Text = "A"
+        ElseIf (NUM4.Text = 11) Then
+            NUM4.Text = "J"
+        ElseIf (NUM4.Text = 12) Then
+            NUM4.Text = "Q"
+        ElseIf (NUM4.Text = 13) Then
+            NUM4.Text = "K"
+        End If
+
+    End Sub
+
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btncount.Click
+        Ans.Items.Clear()
+        If (NUM1.Text = "A") Then
+            NUM1.Text = 1
+        ElseIf (NUM1.Text = "J") Then
+            NUM1.Text = 11
+        ElseIf (NUM1.Text = "Q") Then
+            NUM1.Text = 12
+        ElseIf (NUM1.Text = "K") Then
+            NUM1.Text = 13
+        End If
+        If (NUM2.Text = "A") Then
+            NUM2.Text = 1
+        ElseIf (NUM2.Text = "J") Then
+            NUM2.Text = 11
+        ElseIf (NUM2.Text = "Q") Then
+            NUM2.Text = 12
+        ElseIf (NUM2.Text = "K") Then
+            NUM2.Text = 13
+        End If
+        If (NUM3.Text = "A") Then
+            NUM3.Text = 1
+        ElseIf (NUM3.Text = "J") Then
+            NUM3.Text = 11
+        ElseIf (NUM3.Text = "Q") Then
+            NUM3.Text = 12
+        ElseIf (NUM3.Text = "K") Then
+            NUM3.Text = 13
+        End If
+        If (NUM4.Text = "A") Then
+            NUM4.Text = 1
+        ElseIf (NUM4.Text = "J") Then
+            NUM4.Text = 11
+        ElseIf (NUM4.Text = "Q") Then
+            NUM4.Text = 12
+        ElseIf (NUM4.Text = "K") Then
+            NUM4.Text = 13
+        End If
+        Dim a, b, c, d As Integer
+        a = NUM1.Text
+        b = NUM2.Text
+        c = NUM3.Text
+        d = NUM4.Text
+        If (NUM1.Text = 1) Then
+            NUM1.Text = "A"
+        ElseIf (NUM1.Text = 11) Then
+            NUM1.Text = "J"
+        ElseIf (NUM1.Text = 12) Then
+            NUM1.Text = "Q"
+        ElseIf (NUM1.Text = 13) Then
+            NUM1.Text = "K"
+        End If
+        If (NUM2.Text = 1) Then
+            NUM2.Text = "A"
+        ElseIf (NUM2.Text = 11) Then
+            NUM2.Text = "J"
+        ElseIf (NUM2.Text = 12) Then
+            NUM2.Text = "Q"
+        ElseIf (NUM2.Text = 13) Then
+            NUM2.Text = "K"
+        End If
+        If (NUM3.Text = 1) Then
+            NUM3.Text = "A"
+        ElseIf (NUM3.Text = 11) Then
+            NUM3.Text = "J"
+        ElseIf (NUM3.Text = 12) Then
+            NUM3.Text = "Q"
+        ElseIf (NUM3.Text = 13) Then
+            NUM3.Text = "K"
+        End If
+        If (NUM4.Text = 1) Then
+            NUM4.Text = "A"
+        ElseIf (NUM4.Text = 11) Then
+            NUM4.Text = "J"
+        ElseIf (NUM4.Text = 12) Then
+            NUM4.Text = "Q"
+        ElseIf (NUM4.Text = 13) Then
+            NUM4.Text = "K"
+        End If
+        AnswerN = 0
+        Count1(a, b, c, d)
+        Label1.Text = Ans.Items.Count.ToString
+        If (AnswerN = 0) Then
+            Ans.Items.Add("No answer")
+            Label1.Text = 0
+        End If
+    End Sub
+
+    Private Sub btnclear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        Ans.Items.Clear()
+        Label1.Text = 0
+    End Sub
+
+End Class
